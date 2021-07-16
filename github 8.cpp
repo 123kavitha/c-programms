85bbfe450856e72f66e76332159e477ef085a27b
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-16 20:41:25 +0530

    Add files via upload

---
+++ github 8.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+ int main()
+{
+     char id[10];
+	 int hour;
+	 double value, salary;
+	 printf("Input the Employees ID(Max. 10 chars): ");
+	 scanf("%s", &id);
+	 printf("\ninput the working hrs: ");
+	 scanf("%d", &hour);
+	 printf("\nsalary amoount/hr: ");
+	 scanf("lf", &value);
+	 salary = value * hour;
+	 printf("\nEmployees ID = %s\nSalary = U$ %.21f\n", id,salary);
+	 return(0);
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 	   	
+
+  
