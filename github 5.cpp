51d5e6c6421d4b459bafaa023702aea8317386cd
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-16 20:39:59 +0530

    Add files via upload

---
+++ github 5.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ int main()
+ {
+   int a;
+   long b;
+   short c;
+   float d;
+   long double e;
+   printf("enter int");
+   scanf("%d,&a");
+   printf("a");
+   printf("enter short");
+   scanf("%d,&b");
+   printf("b");
+   printf("enter double");
+   scanf("%lf,&c");
+   printf("c");
+   printf("enter float");
+   scanf("%f,&d");
+   printf("d");
+   printf("enter long double");
+   scanf("%Lf,&e");
+   printf("e");
+ }
