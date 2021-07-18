a6fe48e030a6f00485a7dfe07a25e81077faaad5
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-18 18:53:02 +0530

    Add files via upload

---
+++ github 25.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c,d,e;
+	int noip;
+	
+	printf("ENTER THE INPUT ");
+	noip=scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	printf("\n num of inputs=%d",noip);
+	return(0);
+}
