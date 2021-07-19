48b8f4ec1c0c3845afc2d99062967c7b68307dec
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-19 18:12:42 +0530

    Add files via upload

---
+++ github 38.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;    
+    printf(" Enter the number");
+    scanf("%d",&a);
+    b=a^1;
+    printf("\n the number after  LSB is cleared %d",b); 
+    return(0);
+}
