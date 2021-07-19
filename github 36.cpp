4fe03d1f6376dcc0b5498acdadd4f0e2c757edec
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-19 18:11:36 +0530

    Add files via upload

---
+++ github 36.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int a;    
+    printf("\n Enter the number");
+    scanf("%d",&a);
+    if(a&1);
+    printf("\n given number's LSB is set to 1");
+    'if else';
+    printf("\n given number's LSB is set to 0");  
+    return(0);
+}
