a82bdfec0fd39fc89325814ffa7982b4193228a7
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:06:38 +0530

    Add files via upload

---
+++ github 51.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int a;
+	printf("enter the  age");
+	scanf("%d",&a);
+	if(a>18)
+	printf("person of age =%d is eligible to vote",a);
+    return(0);
+}
