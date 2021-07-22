27931f7bda996594535fe308472188e9506522e8
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:25:04 +0530

    Add files via upload

---
+++ githuub 72.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	char a;
+	printf("enter the value");
+	scanf("%c",&a);
+	if('a'<=a&&a<='z'||'A'<=a&&a<='Z');
+	printf("the given char=%c is an alphabet",a);
+	return(0);
+}
+
+	
+	
