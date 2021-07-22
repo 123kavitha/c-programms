48374c0268095c0f5d949b17e3143f9a901537d3
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 16:10:23 +0530

    Add files via upload

---
+++ github 50.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int a;
+	printf("enter the  number ");
+	scanf("%d",&a);
+	if(a%2!=0);
+	printf("a=%d is odd number",a);
+    return(0);
+}
