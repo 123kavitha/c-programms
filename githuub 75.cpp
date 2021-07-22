2e95126448911e631d95be6435e13fe39dff2741
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:26:29 +0530

    Add files via upload

---
+++ githuub 75.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,m;
+	printf("enter the numbers");
+	scanf("%d",&n);
+	m=1;
+	for(;m<=10;m+=1)		
+	{	
+	printf("\n%d x %d =%d",n,m,n*m);
+	return(0);                                                       
+}
+}
+
+	
+	
