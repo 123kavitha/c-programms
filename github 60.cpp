e4d7cbab57d9a1ae98d8bffd7b80ef7814827551
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:18:44 +0530

    Add files via upload

---
+++ github 60.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    char c;
+	printf("enter the value ");
+	scanf("%c",&c);
+	printf("%d",c);
+	if(65<=c&&c<=90)
+	printf("letter is uppercase");
+	'if else';
+	printf("letter is lowercase");
+    return(0);
+}
