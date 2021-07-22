a2a6b1a62fdd420ffde08630292ba1d252e690e3
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:09:13 +0530

    Add files via upload

---
+++ github 55.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int num;
+	printf("enter the  input");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("the number=%d is even",num);
+	'if else';
+	printf("the number=%d is odd",num);
+    return(0);
+}
