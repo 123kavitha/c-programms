84020061e80b93ea397e3d7be9e5689809023a1c
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:26:58 +0530

    Add files via upload

---
+++ githuub 76.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	printf("\n enter the numbers");
+	scanf("%d",&a);
+	if(a&1)
+	printf("\n num=%d is odd",a);
+	'if else';			
+	printf("\n num=%d is even",a);
+	return(0);                                                       
+}
+
+	
+	
