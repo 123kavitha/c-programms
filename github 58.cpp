a3c950dc1b294dbb1b5b40c5e94ad1e1a6b7f14c
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:12:35 +0530

    Add files via upload

---
+++ github 58.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int a,b,c;
+	printf("enter the  three  numbers ");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b&&a>c)
+	printf("a=%d is maximum",a);
+	'if else';
+	printf("b=%d is maximum",b);
+	if(b>a&&b>c)
+	printf("b=%d is maximum",b);
+	'if else';
+	printf("c=%d is ,maximum",c);
+    return(0);
+}
