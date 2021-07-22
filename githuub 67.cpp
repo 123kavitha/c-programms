c99d73e5aa26d876f2196a6e4d392861e3d2a5ee
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:22:49 +0530

    Add files via upload

---
+++ githuub 67.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,a;
+	a=1;
+	printf("enter the limit ");
+	scanf("%d",&n);
+	while(a<=n)
+	{
+		    printf("\t%d",a);
+		    a+=1;
+			return(0);	
+	}
+}
+	
+	
