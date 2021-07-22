359223826a0823500e2c5b990236b3dd38724a8d
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 16:10:03 +0530

    Add files via upload

---
+++ github 49.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int num;
+	printf("enter the  input ");
+	scanf("%d",&num);
+	if(num>0);
+	printf("the num =%d is positive",num);
+	'if else';
+    printf("the num=%d is negative ",num);
+    return(0);
+}
