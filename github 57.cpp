c2175f99f89d5efcd7c8236b644d2f2ac7038c01
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:11:09 +0530

    Add files via upload

---
+++ github 57.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int a,b;
+	printf("enter the  two numbers ");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("a=%d is maximum",a);
+	'if else';
+	printf("b=%d is maximum",b);
+    return(0);
+}
