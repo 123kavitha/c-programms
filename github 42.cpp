070ed227491cedc3a7f9cc15356db4ae0c650be2
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 16:07:16 +0530

    Add files via upload

---
+++ github 42.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+	printf("enter  num ");
+	scanf("%d",&a);
+	printf("enter other num");
+	scanf("%d",&b);
+	(a>b)?printf("max=%d",a):printf("max=%d",b);
+     return(0);
+}
