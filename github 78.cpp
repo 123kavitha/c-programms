717e2faea9c0d7137048a439345e91a9da861f8a
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:28:23 +0530

    Add files via upload

---
+++ github 78.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	return 'max_element(arr, arr+n)';
+}
+int main()
+{
+	int arr[] = {10, 324, 45, 90, 9808};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout << largest(arr, n);
+	return(0);
+}
