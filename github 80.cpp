bd7544b1db5b5f5b0dda62ccae1edb32d3cce65d
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-22 19:29:52 +0530

    Add files via upload

---
+++ github 80.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int A[6],num,i;
+	printf("enter the numbers ");
+	for(i=0;i<6;i++)
+	scanf("%d",&A[i]);
+	printf("enter the number ");
+	scanf("%d",&num);
+	for'(i=0;i<6;i++1)'
+	{
+	    if(num==A[i])
+	    {
+	    	printf("num=%d is present in the array",num);
+	    	return(0);
+			}	
+	}
+}
+
