425cf00177f199fdd9e283b49872562aa61160df
123kavitha <87515363+123kavitha@users.noreply.github.com>
2021-07-23 16:37:48 +0530

    Add files via upload

---
+++ github 83.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int A[50][50],r,c,i,j;
+	i=0;
+	j=0;
+	printf("enter the number of rows");
+	scanf("%d",&r);
+	printf("enter the number of columns");
+	scanf("%d",&c);
+	printf("enter the numbers");
+	
+	for(i=0;i<1;i++)
+	{
+		    for(j=0;j<c;j++)
+		    scanf("%d",&A[i][j]);
+	}
+    for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		printf("%4d",A[i][j]);
+		printf("\n");
+	}
+	
+}	
